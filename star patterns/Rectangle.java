941054938c67efa1ed1a51df6345034e8bd4de06
mayankpatidar18-ram <mayankpatidar989@gmail.com>
2025-11-21 02:11:27 +0530

    added java patterns

---
+++ star patterns/Rectangle.java
@@ -0,0 +1,16 @@
+class Rectangle{
+    public static void main(String[]args){
+int n=3;
+int m=5;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+
+    
+    System.out.print("*");
+    }
+    System.out.println();
+
+}
+    }
+
+}
